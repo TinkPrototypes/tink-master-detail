f9979f4f641ffec796eafc21b5bf0fbb0c8b0a76
Vincent Bouillart <vincent.bouillart@appfoundry.be>
2015-06-05 15:20:07 +0200

    resizing

--- src/scripts/directives/tinkMasterDetailView.js
+++ src/scripts/directives/tinkMasterDetailView.js
@@ -6,7 +6,7 @@
  		transclude:'true',
  		scope:true,
  		controller:function(){
- 			var $element={listView:undefined,contentView:undefined};
+ 			var $element={listView:undefined,contentView:undefined,};
  			this.setListView = function(element){
  				$element.listView = element;
  			}
@@ -15,9 +15,34 @@
  			}
  		},
  		link:function(scope,elem,attr,ctrl){
+
  			 scope.$on('$destroy',function handleDestroyEvent() {
  			 	//handle destory stuff unbind etc
  			 });
+ 			 scope.addResizer = function(){
+ 			 	var x = e.pageX - $('#sidebar').offset().left;
+        if (x > min && x < max && e.pageX < ($(window).width() - mainmin)) {  
+          $('#sidebar').css("width", x);
+          $('#main').css("margin-left", x);
+        }
+ 			 }
+
+ 			 /* //Hier komt het op neer :P 
+					$('#split-bar').mousedown(function (e) {
+					    e.preventDefault();
+					    $(document).mousemove(function (e) {
+					        e.preventDefault();
+					        var x = e.pageX - $('#sidebar').offset().left;
+					        if (x > min && x < max && e.pageX < ($(window).width() - mainmin)) {  
+					          $('#sidebar').css("width", x);
+					          $('#main').css("margin-left", x);
+					        }
+					    })
+					});
+					$(document).mouseup(function (e) {
+					    $(document).unbind('mousemove');
+					});
+ 			 */
  		}
  	}
  }]);
